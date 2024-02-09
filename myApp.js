e0745450db95398bec819fb7645ed706f0e52e89
vgeroim <vgeroim.qa@gmail.com>
2024-02-08 21:35:12 -0500

    Serve index.html

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 let express = require('express');
 let app = express();
+const absolutePath = __dirname + '/views/index.html';
 
-console.log("Hello World");
 
 app.get('/', function (req, res) {
-    res.send('Hello Express');
+    res.sendFile(absolutePath);
 })
 
 
