3dec2628abda0cfc5c1f0e042298ab2ac663b1d6
vgeroim <vgeroim.qa@gmail.com>
2024-02-09 21:49:22 -0500

    add middleware chain

--- myApp.js
+++ myApp.js
@@ -26,6 +26,13 @@ app.get('/json', function (req, res) {
     res.json(obj);
 })
 
+app.get('/now', function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function (req, res) {
+    res.json({ time: req.time })
+})
+
 
 
 
