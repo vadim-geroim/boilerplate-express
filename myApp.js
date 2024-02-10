ba346e07d8a14336355ae341db38757efab7a63c
vgeroim <vgeroim.qa@gmail.com>
2024-02-09 19:55:58 -0500

    add styles to App

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
 const absolutePath = __dirname + '/views/index.html';
+const pathToAssets = __dirname + '/public';
 
+app.use(express.static(pathToAssets));
 
 app.get('/', function (req, res) {
     res.sendFile(absolutePath);
