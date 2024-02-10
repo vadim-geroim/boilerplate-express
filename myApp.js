8fe614a9956ae38fd021c8bf6caba9501668621a
vgeroim <vgeroim.qa@gmail.com>
2024-02-09 20:02:28 -0500

    update path to the file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
 let express = require('express');
 let app = express();
 const absolutePath = __dirname + '/views/index.html';
-const pathToAssets = __dirname + '/public';
 
-app.use(express.static(pathToAssets));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(absolutePath);
