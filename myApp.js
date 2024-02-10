4d12e2bd9f305278abdd708aca34aa813001bf89
vgeroim <vgeroim.qa@gmail.com>
2024-02-10 13:09:30 -0500

    add segment route

--- myApp.js
+++ myApp.js
@@ -33,6 +33,11 @@ app.get('/now', function (req, res, next) {
     res.json({ time: req.time })
 })
 
+app.get('/:word/echo', function (req, res) {
+    const word = req.params.word;
+    res.json({ echo: word });
+})
+
 
 
 
